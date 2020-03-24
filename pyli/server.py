211b865719229e09da481e3b9dfe367aab999e4d
munagekar <avm.abhishek@gmail.com>
2020-03-24 16:59:07 +0900

    Fix Sign up

--- pyli/server.py
+++ pyli/server.py
@@ -1,5 +1,7 @@
-from typing import Dict
+import urllib.parse
+from typing import Any, Dict
 
+import requests
 import toml
 import typer
 from typer import Option
@@ -15,25 +17,61 @@ SERVER_ADDR = "server_addr"
 SERVER_USER = "user"
 
 
+def get_current_server() -> str:
+    conf = load_config()
+    return conf[CURRENT_SERVER]
+
+
+def load_config() -> Dict[str, Any]:
+    return toml.loads(CONF_FILE.read_text())  # type: ignore
+
+
+def get_server_config(server_name: str) -> Dict[str, str]:
+    conf = load_config()
+    server_conf = conf[SERVER_CONF_KEY][server_name]
+    return server_conf
+
+
+def get_current_server_config() -> Dict[str, str]:
+    return get_server_config(get_current_server())
+
+
 @app.command()
-def add(server_name: str, addr: str = Option(...,), user: str = Option(...)):
+def add(server_name: str, addr: str = Option(...,)):
     """
     Adds the Server to Pylinks-Cli
 
     Args:
         server_name: The name to used for testing
         addr: Address of the server. Eg https://localhost.com
-        user: Username to be used with the server
     """
-    conf = toml.loads(CONF_FILE.read_text())
+    conf = load_config()
     conf[CURRENT_SERVER] = server_name
 
     CONF_FILE.write_text(toml.dumps(conf))
 
-    server_config = {SERVER_ADDR: addr, SERVER_USER: user}
+    server_config = {SERVER_ADDR: addr}
 
     servers: Dict[str, Dict[str, str]] = conf.get(SERVER_CONF_KEY, {})
     servers[server_name] = server_config
     conf[SERVER_CONF_KEY] = servers
 
     CONF_FILE.write_text(toml.dumps(conf))
+
+
+@app.command()
+def signup(user: str = Option(..., "-u"), password: str = Option(..., "-p")):
+    """
+    Sign up on the Link Server
+
+    Args:
+        user : Username
+        password: Password
+    """
+    server_config = get_current_server_config()
+    server_address = server_config[SERVER_ADDR]
+
+    endpoint = urllib.parse.urljoin(server_address, "/user/")
+    data = f'{{"username":"{user}","password":"{password}"}}'
+    resp = requests.post(endpoint, data=data)
+    print(resp)
