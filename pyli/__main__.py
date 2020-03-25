426cec85349f9313f7cc86b0ec987d749fa37a85
munagekar <avm.abhishek@gmail.com>
2020-03-25 18:40:17 +0900

    Add Magic Link Join

--- pyli/__main__.py
+++ pyli/__main__.py
@@ -1,13 +1,30 @@
+import urllib.parse
+
+import requests
 import typer
+from typer import Option
 
 import pyli.conf as conf
 import pyli.create as create
 import pyli.server as server
+from pyli.server import SERVER_ADDR, get_current_server_config, get_token
 
 app = typer.Typer()
 app.add_typer(create.app, name="create")
 app.add_typer(server.app, name="server")
 app.add_typer(conf.app, name="conf")
+
+
+@app.command()
+def join(invite_id: str = Option(..., prompt=True)):
+    server_config = get_current_server_config()
+    auth_header = get_token()
+    headers = {"Authorization": auth_header}
+    params = (("link_id", invite_id),)
+    endpoint = urllib.parse.urljoin(server_config[SERVER_ADDR], "/invite")
+    response = requests.get(url=endpoint, headers=headers, params=params)  # type:ignore
+    print(response.status_code)
+    print(response.text)
 
 
 def main():
--- pyli/create.py
+++ pyli/create.py
@@ -8,6 +8,16 @@ from pyli.server import SERVER_ADDR, get_current_server_config, get_token
 
 app = typer.Typer()
 
+ROLES = ["reader", "writer", "admin"]
+
+
+def complete_roles(incomplete: str):
+    completion = []
+    for name in ROLES:
+        if name.startswith(incomplete):
+            completion.append(name)
+    return completion
+
 
 @app.command()
 def link(url: str = Option(..., prompt=True), text: str = Option(..., prompt=True)):
@@ -22,12 +32,29 @@ def link(url: str = Option(..., prompt=True), text: str = Option(..., prompt=Tru
 
 
 @app.command()
-def team(teamname: str = Option(..., prompt=True)):
+def team(teamname: str):
     server_config = get_current_server_config()
     auth_header = get_token()
     headers = {"Authorization": auth_header}
     params = (("teamname", teamname),)
     endpoint = urllib.parse.urljoin(server_config[SERVER_ADDR], "/team")
+    response = requests.post(url=endpoint, headers=headers, params=params)
+    print(response.status_code)
+    print(response.text)
+
+
+@app.command()
+def invite(teamname: str = Option(..., prompt=True), role: str = Option(..., prompt=True)):
+    server_config = get_current_server_config()
+    auth_header = get_token()
+    role = role.lower()
+    headers = {"Authorization": auth_header}
+    endpoint = urllib.parse.urljoin(server_config[SERVER_ADDR], "/invite")
+
+    params = (
+        ("teamname", teamname),
+        ("role", role),
+    )
     response = requests.post(url=endpoint, headers=headers, params=params)
     print(response.status_code)
     print(response.text)
